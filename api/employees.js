b4a9dfc9d32428a2fa3805f7f6ef9414d00cef43
CooperHoag <cooperhoag@gmail.com>
2025-06-27 06:35:11 -0700

    POST /employees tests

--- api/employees.js
+++ api/employees.js
@@ -1,5 +1,20 @@
 import express from "express";
+import { getEmployees, createEmployee } from "#db/queries/employees";
 const router = express.Router();
 export default router;
 
 // TODO: this file!
+router.route("/").get(async (req, res) => {
+  const employees = await getEmployees();
+  res.send(employees);
+});
+
+router.route('/').post(async (req, res) => {
+  console.log('BODY', req.body);
+  if (!req.body || !req.body.name || !req.body.birthday || !req.body.salary) {
+    res.status(400).send('No body given');
+  }else {
+    const newEmployee = await createEmployee(req.body);
+    res.status(201).send(newEmployee);
+  }
+});
--- app.js
+++ app.js
@@ -7,3 +7,12 @@ app.use(express.json());
 
 import router from './api/employees.js'
 app.use('/employees', router);
+
+app.get('/', (req, res) => {
+  res.send("Welcome to the FullStack Employees API")
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong :(");
+});
